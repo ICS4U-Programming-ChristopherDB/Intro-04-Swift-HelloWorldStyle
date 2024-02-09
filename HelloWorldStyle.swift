5395003bd8e78d5a36286fbd93fa666d00d6bdd3
Christopher Di Bert <christopher.di-bert@stu.ocsb.ca>
2024-02-09 15:32:45 +0000

    Intial commit

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,10 @@
+// Copyright (c) 2024 Chris Di Bert All rights reserved.
+// .
+// Created by: Christopher Di Bert
+// Date : Feb.7, 2024
+// This program prints hello to my friend and prints
+// a face
+
+// Prints text
+print("Hey, Spencer!")
+print("Here's a funny face: ( ͡° ʖ ͡°)")
